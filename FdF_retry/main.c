65a82334c3c05aac30507721d9a78ebb2962e3c4
Brenton MOODLEY <bmoodley@e4r16p11.wethinkcode.co.za>
2018-01-03 15:54:33 +0200

    added some skeletal pseudo

--- FdF_retry/main.c
+++ FdF_retry/main.c
@@ -1,6 +1,17 @@
 #include <fcntl.h>
 #include "libft/includes/libft.h"
 
+ft_isgoodlen()
+//checks if every subsequent line is equal to or longer than the first
+
+ft_usage()
+//checks if file exists, correct number of params are used
+
+ft_readin()
+//allocates memory internally
+//reads entire file into internally allocated memory
+//might have to split this one
+
 int		main(int argc, char **argv)
 {
 	char	*line;
@@ -8,19 +19,13 @@ int		main(int argc, char **argv)
 
 	line = NULL;
 	fd = open(argv[1], O_RDONLY);
-	//if (argc != 2)
-	//	throw error;
 	ft_putstr("1\n");
 	while (get_next_line(fd, &line) > 0)
-	{
 		ft_putendl(line);
-	}
 	ft_putstr("2\n");
 	close(fd);
-	fd = open(argv[1], O_RDONLY);	
+	fd = open(argv[1], O_RDONLY);
 	while (get_next_line(fd, &line) > 0)
-	{
 		ft_putendl(line);
-	}
 	return (0);
 }
